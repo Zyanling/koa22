74b68cb1f021d6847f08ac53e6d6d233cde48d92
yanling.zhang <371947151@qq.comm>
2019-09-20 09:23:05 +0800

    fix

---
+++ koa.md
@@ -0,0 +1,10 @@
+const http=require("http");  http服务于客户端模块
+const path=require("path");   // 该模块提供了公寓处理文件与目录的路径的工具函数
+const url=require("url");    //该模块用于url处理与解析
+const fs=require("fs");      //文件系统模块
+const hostname="127.0.0.1";
+
+
+npm 是模块管理工具
+nvm 控制nodejs版本
+
--- src/page/config/data/index.js
+++ src/page/config/data/index.js
@@ -8,12 +8,30 @@ const styles = (theme) => ({
 })
 class Component extends React.Component {
 
-    componentDidMount(){
-        
+    constructor(props) {
+        super(props);
+        this.state = { num: 1 }
+        this.handleClick=this.handleClick.bind(this)
+    }
+    componentDidMount() {
+
+    }
+
+    handleClick = () => {
+        this.setState({ num: this.state.num + 1 },()=>{
+            console.log(1)
+        })
     }
     render() {
+    
         return (
             <>
+                {this.state.num}
+                <button
+                    onClick={this.handleClick}
+                >
+                    add
+                </button>
                 data indexrrwrrr
             </>
         )
